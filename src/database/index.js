b9daa4a6e2a44b49ef6b840f399bf4b60eda48e1
Walefe <walefedev@gmail.com>
2020-01-28 14:39:39 -0300

    Chore: create model user and loading in database.

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,10 @@ import Sequelize from 'sequelize';
 
 import databaseConfig from '../config/database';
 
+import User from '../app/models/User';
+
+const models = [User];
+
 class Database {
   constructor() {
     this.init();
@@ -9,6 +13,8 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
   }
 }
 
