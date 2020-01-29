819052fe76480cd0a83f3d8a5b2f7c6a8e2b45fd
Walefe <walefedev@gmail.com>
2020-01-29 13:15:47 -0300

    Chore: add session controller and route session for authenticate.

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import SessionsController from './app/controllers/SessionController';
+
 const routes = new Router();
+
+routes.post('/sessions', SessionsController.store);
 
 export default routes;
