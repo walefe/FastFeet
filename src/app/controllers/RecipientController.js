132390b513db48c4db2d0c6b2ba7df16665f055f
Walefe <walefedev@gmail.com>
2020-01-29 14:53:19 -0300

    Chore: add controller and routes for recipients.

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,59 @@
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const recipientExists = await Recipient.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists.' });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(401).json({ error: 'Recipient not found.' });
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await recipient.update(req.body);
+
+    return res.json({ id, name, street, number, complement, state, city, cep });
+  }
+}
+
+export default new RecipientController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
 import SessionsController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionsController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
