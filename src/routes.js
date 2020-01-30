185d153d4d7a6ed411fd8ab4f62d0d5eae50258b
Walefe <walefedev@gmail.com>
2020-01-30 17:01:17 -0300

    Chore: create routes for controller delivery.

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionsController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliveryController from './app/controllers/DeliveryController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,10 @@ routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.get('/deliveries', DeliveryController.index);
+routes.post('/deliveries', DeliveryController.store);
+routes.put('/deliveries/:id', DeliveryController.update);
+routes.delete('/deliveries/:id', DeliveryController.delete);
 
 export default routes;
