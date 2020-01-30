587763bcc901ea809cbef4e4dac9a64766fb0ccb
Walefe <walefedev@gmail.com>
2020-01-30 09:27:35 -0300

    Chore: add schema validade in controller recipient.

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,13 +1,28 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validate fails.' });
+    }
     const recipientExists = await Recipient.findOne({
       where: { name: req.body.name },
     });
 
     if (recipientExists) {
-      return res.status(400).json({ error: 'Recipient already exists.' });
+      return res.status(400).json({ error: 'Recipient already exists' });
     }
 
     const {
@@ -21,19 +36,23 @@ class RecipientController {
       cep,
     } = await Recipient.create(req.body);
 
-    return res.json({
-      id,
-      name,
-      street,
-      number,
-      complement,
-      state,
-      city,
-      cep,
-    });
+    return res.json({ id, name, street, number, complement, state, city, cep });
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      cep: Yup.number().positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validate fails.' });
+    }
     const { id } = req.params;
 
     const recipient = await Recipient.findByPk(id);
