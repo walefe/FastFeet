b1b542f73899f8841efd3e00f9aa6a4c348d3cf6
Walefe <walefedev@gmail.com>
2020-01-30 09:28:15 -0300

    Chore: add schema validate in controller session and verify user is admin.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,16 +1,31 @@
 import jwt from 'jsonwebtoken';
 
+import * as Yup from 'yup';
+
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validate fails.' });
+    }
+
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email, admin: true } });
 
     if (!user) {
-      return res.status(401).json({ error: 'User not found!' });
+      return res.status(400).json({ error: 'User must be an administrator.' });
     }
 
     if (!(await user.checkPassword(password))) {
