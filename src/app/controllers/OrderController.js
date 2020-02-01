6d247ebf7d0ce9633e78ab8a910bc8d7f25f77f3
Walefe <walefedev@gmail.com>
2020-02-01 20:39:37 -0300

    Chore: create controller order and routers for order,

---
+++ src/app/controllers/OrderController.js
@@ -0,0 +1,96 @@
+import * as Yup from 'yup';
+
+import Recipient from '../models/Recipient';
+import Delivery from '../models/Delivery';
+import Order from '../models/Order';
+
+class OrderController {
+  async index(req, res) {
+    const orders = await Order.findAll();
+
+    if (!orders) {
+      return res.status(401).json({ error: 'Orders not found' });
+    }
+
+    return res.json(orders);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number().required(),
+      deliveryman_id: Yup.number().required(),
+      product: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const deliverytExists = await Delivery.findOne({
+      where: { id: deliveryman_id },
+    });
+
+    if (!deliverytExists) {
+      return res.status(400).json({ error: 'Delivery not exists.' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { id: recipient_id },
+    });
+
+    if (!recipientExists) {
+      return res.status(400).json({ error: 'Recipient not exists.' });
+    }
+
+    const { id, product } = await Order.create(req.body);
+
+    return res.json({
+      id,
+      recipient_id,
+      deliveryman_id,
+      product,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number(),
+      deliveryman_id: Yup.number(),
+      product: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { id } = req.params;
+
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      return res.status(400).json({ error: 'Order not found' });
+    }
+
+    await order.update(req.body);
+
+    return res.json(order);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      return res.status(400).json({ error: 'Order not found' });
+    }
+
+    await Order.destroy({ where: { id } });
+
+    return res.status(200).json();
+  }
+}
+
+export default new OrderController();
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import SessionsController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliveryController from './app/controllers/DeliveryController';
+import OrderController from './app/controllers/OrderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,5 +20,10 @@ routes.get('/deliveries', DeliveryController.index);
 routes.post('/deliveries', DeliveryController.store);
 routes.put('/deliveries/:id', DeliveryController.update);
 routes.delete('/deliveries/:id', DeliveryController.delete);
+
+routes.get('/orders', OrderController.index);
+routes.post('/orders', OrderController.store);
+routes.put('/orders/:id', OrderController.update);
+routes.delete('/orders/:id', OrderController.delete);
 
 export default routes;
