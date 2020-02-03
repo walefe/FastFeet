5cf4d32793f94566e3e7267a747f1ce4ca4f1201
Walefe <walefedev@gmail.com>
2020-02-02 21:15:10 -0300

    Chore: add config mailTrap.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '641ddf5a66717f',
+    pass: '8793bf8076efeb',
+  },
+  default: {
+    from: 'Equipe FasFeet <noreply@fastfeet.com>',
+  },
+};
